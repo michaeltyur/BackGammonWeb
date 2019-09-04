255b88cba73679952e0692ba31721a640fdb067e
Bob Bobov <bobyy@yandex.ru>
2019-09-04 22:41:13 +0300

    add common

---
+++ BackGammonWebSolution/Common/Data/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Data
+{
+    [DataContract]
+    public class Message
+    {
+        [DataMember]
+        public int MessageId { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string Content { get; set; }
+
+        [DataMember]
+        public DateTime Date { get; set; }
+
+
+    }
+}
---
+++ BackGammonWebSolution/Common/Data/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Data
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int UserId { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+       // public ClaimsIdentity Username { get; set; }
+        public string Password { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public bool IsOnline { get; set; }
+
+        public string SignalRConnectionID { get; set;}
+
+
+    }
+
+}
